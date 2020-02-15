c0ac981b48037fe115f8c42adda2b16f97d52c16
Matt <mblanco1000@gmail.com>
2020-02-15 09:37:49 -0500

    AutoCases command groups + supporting commands
    Added command groups for the 13 (currently) auto cases, and added
    command groups for launching 3 balls in succession (aka auto) for the
    low and high goals.

---
+++ src/main/java/frc/robot/commands/AutoCases/case1.java
@@ -0,0 +1,65 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.AutoCases;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.commands.TurnAngle;
+import frc.robot.commands.LowShoot3;
+import frc.robot.commands.DriveDistance;
+
+public class case1 extends CommandGroup {
+  /**
+   * Add your docs here.
+   */
+  public case1() {
+    // Add Commands here:
+    // e.g. addSequential(new Command1());
+    // addSequential(new Command2());
+    // these will run in order.
+
+    addSequential(new TurnAngle(235));
+    addSequential(new DriveDistance(144));
+
+    addSequential(new TurnAngle(180));
+    addSequential(new DriveDistance(20));
+    addSequential(new LowShoot3());
+
+    /*addSequential(new LowGoalShoot());
+    addParallel(new LoadCells());
+    try {
+      Thread.sleep(1000, 0);
+    } catch (InterruptedException e1) {
+      // TODO Auto-generated catch block
+      e1.printStackTrace();
+    }
+    
+    for (int i = 0; i < 3; i++) {
+      addSequential(new FeedShooter());
+      try {
+        Thread.sleep(500, 0);
+      } catch (InterruptedException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+    //speed down shooter? */
+    
+
+    // To run multiple commands at the same time,
+    // use addParallel()
+    // e.g. addParallel(new Command1());
+    // addSequential(new Command2());
+    // Command1 and Command2 will run in parallel.
+
+    // A command group will require all of the subsystems that each member
+    // would require.
+    // e.g. if Command1 requires chassis, and Command2 requires arm,
+    // a CommandGroup containing them would require both the chassis and the
+    // arm.
+  }
+}
